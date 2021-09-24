7a71fd5bdee3c355d02ae183f1080369b1d92528
Brett Cannon <brett@python.org>
2021-09-24 16:01:25 -0700

    Move fish_greeting to its own file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,28 +25,3 @@ end
 if command --query zoxide
   zoxide init fish | source
 end
-
-function tool_mapping -d "Print what an old command should be replaced with"
-  set_color red
-  echo -n $argv[1]
-  set_color normal
-  echo -n " → "
-  if command --query $argv[2]
-    set_color green
-    echo $argv[2]
-  else
-    set_color yellow
-    echo -n $argv[2]
-    set_color normal
-    echo "?"
-  end
-  set_color normal
-end
-
-function fish_greeting
-  tool_mapping cd zoxide
-  tool_mapping ls exa
-  tool_mapping cat bat
-  tool_mapping top btm
-  tool_mapping du dust
-end
