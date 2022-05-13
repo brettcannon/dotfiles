b8dc4bfab2098162af7514ddb9d3c0a33fc73a05
Brett Cannon <brett@python.org>
2022-05-13 13:34:20 -0700

    Track `py-venv` in the right place

---
+++ .config/fish/functions/py-venv.fish
@@ -0,0 +1,3 @@
+function py-venv --description "`py -m venv .venv`"
+    py -m venv .venv
+end
--- py-venv.fish
+++
@@ -1,3 +0,0 @@
-function py-venv -d "Create a virtual environment named `.venv` using `py`"
-    py -m venv .venv
-end
