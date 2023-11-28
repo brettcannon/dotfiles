6b46b692f9352fafc1b65cef13d51e7feb89771f
Brett Cannon <brett@python.org>
2023-11-28 10:46:59 -0800

    Add `--yes` to `apt full-upgrade`

--- .config/fish/functions/update-os.fish
+++ .config/fish/functions/update-os.fish
@@ -2,16 +2,16 @@ function update-os --description "Update the OS"
     echo "Linux distro ..."
     if test -e /etc/os-release
         if grep --quiet leap /etc/os-release
-            echo "No support OpenSUSE LEAP (yet)"
+            echo "No support for OpenSUSE LEAP"
             return 1
         else if grep --quiet tumbleweed /etc/os-release
             # Python dependencies: zypper info -t pattern devel_basis
             sudo zypper --non-interactive dist-upgrade
-        # Covers Ubuntu as well thanks to `ID_LIKE=debian`.
+        # Debian covers Ubuntu as well thanks to `ID_LIKE=debian`.
         else if grep --quiet debian /etc/os-release
             # https://devguide.python.org/getting-started/setup-building/#install-dependencies
             # https://wiki.debian.org/InstallingDebianOn/Microsoft/Windows/SubsystemForLinux
-            sudo apt update && sudo apt full-upgrade
+            sudo apt update && sudo apt full-upgrade --yes
         else
             # dnf builddep python3
             echo "Unsupported Linux distro"
