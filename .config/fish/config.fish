edef82b834b65ad15542057d9da0d2ba99492eae
Brett Cannon <brett@python.org>
2023-12-11 21:01:10 -0800

    Alias `nvim` to `vim`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,6 +25,10 @@ else if test -e ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.soc
   set -g -x SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 end
 
+if command --search nvim
+  alias vim nvim
+end
+
 if command --search code-insiders > /dev/null
   set -g -x EDITOR "code-insiders --wait"
 else if command --search code > /dev/null
@@ -35,10 +39,6 @@ end
 
 if command --search starship > /dev/null
   starship init fish | source
-end
-
-if command --search zoxide > /dev/null
-  zoxide init fish | source
 end
 
 if string match -q "$TERM_PROGRAM" "vscode"
