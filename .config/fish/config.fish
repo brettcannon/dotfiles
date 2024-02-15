784fa8afaf0206cd0b6fee30256f9e2e598a3578
Brett Cannon <brett@python.org>
2024-02-15 10:23:47 -0800

    Move interactive shell configurations to their own `if status is-interactive` block

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@
 # `set`: http://fishshell.com/docs/current/cmds/set.html?highlight=set
 # `fish_add_path`: http://fishshell.com/docs/current/cmds/fish_add_path.html
 
-set -g -x CLICOLOR 1
 set -g -x CC clang
 set -g -x CXX clang++
 # https://devguide.python.org/getting-started/setup-building/#clang
@@ -13,56 +12,60 @@ set -g -x PIP_RESPECT_VIRTUALENV 1
 # Move to os.process_cpu_count() once Python 3.13 is the oldest.
 set -g -x MAKEFLAGS (python3 -c "import os; print(f'-j{os.cpu_count()}')")
 
-set -g -x CDPATH ~/Repositories
-
 # Must come before anything relying on `PATH`.
 if test -e /home/linuxbrew/.linuxbrew/bin/brew
   set -g -x HOMEBREW_NO_ENV_HINTS 1
   /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
 end
 
-if test -e ~/.1password/agent.sock
-  set -g -x SSH_AUTH_SOCK ~/.1password/agent.sock
-else if test -e ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-  set -g -x SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-end
-
 if test -e ~/.cargo/bin
    fish_add_path -g ~/.cargo/bin
 end
 
-if command --search nvim > /dev/null
-  alias vim nvim
-  echo "vim → nvim"
-end
 
-if command --search lsd > /dev/null
-  alias ls lsd
-  echo "ls → lsd"
-end
+if status is-interactive
+  set -g -x CLICOLOR 1
+  set -g -x CDPATH ~/Repositories
 
-if command --search code-insiders > /dev/null
-  set -g -x EDITOR "code-insiders --wait"
-else if command --search code > /dev/null
-  set -g -x EDITOR "code --wait"
-else
-  set -g -x EDITOR nano
-end
-
-if command --search starship > /dev/null
-  starship init fish | source
-end
-
-if string match -q "$TERM_PROGRAM" "vscode"
-  if command --search code-insiders > /dev/null
-    set -x CODE_ACTIVATION (code-insiders --locate-shell-integration-path fish)
-  else if command --search code > /dev/null
-    set -x CODE_ACTIVATION (code --locate-shell-integration-path fish)
+  if command --search starship > /dev/null
+    starship init fish | source
   end
 
-  if set -q CODE_ACTIVATION
-    source $CODE_ACTIVATION
+  if test -e ~/.1password/agent.sock
+    set -g -x SSH_AUTH_SOCK ~/.1password/agent.sock
+  else if test -e ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+    set -g -x SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+  end
+
+  if command --search nvim > /dev/null
+    alias vim nvim
+    echo "vim → nvim"
+  end
+
+  if command --search lsd > /dev/null
+    alias ls lsd
+    echo "ls → lsd"
+  end
+
+  if command --search code-insiders > /dev/null
+    set -g -x EDITOR "code-insiders --wait"
+  else if command --search code > /dev/null
+    set -g -x EDITOR "code --wait"
   else
-    echo "Unable to find VS Code shell integration."
+    set -g -x EDITOR vim
+  end
+
+  if string match -q "$TERM_PROGRAM" "vscode"
+    if command --search code-insiders > /dev/null
+      set -x CODE_ACTIVATION (code-insiders --locate-shell-integration-path fish)
+    else if command --search code > /dev/null
+      set -x CODE_ACTIVATION (code --locate-shell-integration-path fish)
+    end
+
+    if set -q CODE_ACTIVATION
+      source $CODE_ACTIVATION
+    else
+      echo "Unable to find VS Code shell integration."
+    end
   end
 end
