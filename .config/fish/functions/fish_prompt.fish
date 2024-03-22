228e0d305961b75fa58bd208c742e218df0cf6d3
Brett Cannon <brett@python.org>
2024-03-22 15:33:44 -0700

    Set fish prompt to put Git info on its own line

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,17 +1,22 @@
 if not command --search starship > /dev/null
     function fish_prompt
+        set -g __fish_git_prompt_show_informative_status 'yes'
         set -g __fish_git_prompt_showcolorhints 'yes'
         set -g __fish_git_prompt_color_branch yellow
-        set -g __fish_git_prompt_showdirtystate 'yes'
-        set -g __fish_git_prompt_showuntrackedfiles 'yes'
+        set -g __fish_git_prompt_color_cleanstate green
 
-        set_color normal
-        if __fish_git_prompt
-            echo -n " "
+        if fish_git_prompt
+            echo
         end
 
-        set_color -o red
-        echo -n "> "
+        set -l last_status $status
+        set -l stat
+        if test $last_status -ne 0
+            set_color -o red
+        else
+           set_color -o green
+        end
+        echo -n "➲ "
         set_color normal
     end
 end
